76a110d36d33e3dab23db6e79d477b60e9443e25
chtnym9 <46001824+chtnym9@users.noreply.github.com>
2020-04-19 11:39:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,13 @@
 const WebSocket = require('ws');
  
-const ws = new WebSocket('wss://echo.websocket.org/', {
-  origin: 'https://websocket.org'
-});
+const wss = new WebSocket.Server({ port: 8080 });
  
-ws.on('open', function open() {
-  console.log('connected');
-  ws.send(Date.now());
-});
- 
-ws.on('close', function close() {
-  console.log('disconnected');
-});
- 
-ws.on('message', function incoming(data) {
-  console.log(`Roundtrip time: ${Date.now() - data} ms`);
- 
-  setTimeout(function timeout() {
-    ws.send(Date.now());
-  }, 500);
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(data) {
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
+  });
 });
